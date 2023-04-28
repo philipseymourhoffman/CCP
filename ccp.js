e4dfeeebaa58da6780a7bf354e47a8c4af731017
philipseymourhoffman <131834905+philipseymourhoffman@users.noreply.github.com>
2023-04-27 21:46:35 -0400

    Update ccp.js

--- ccp.js
+++ ccp.js
@@ -20,7 +20,7 @@
 
     // Target Users: YOU
     // Enter your username here to change your color
-    const targetUsers1 = ['PhilipSeymourHoffman'];
+    const targetUsers1 = ['YOURUSERNAMEHERE'];
     // Customize the color changing the hexcode to your preferred color
     const targetColor1 = '#ff007f';
     // Set to true if you want to change the color of your own chat messages.
@@ -28,7 +28,7 @@
 
     // Target Users: FRIEND LIST
     // Enter the usernames of your friends here to change their color.
-    const targetUsers2 = ['GroyperGosling', 'Exousia'];
+    const targetUsers2 = ['FRIEND1', 'FRIEND2', 'FRIEND3'];
     // Customize the color changing the hexcode to your preferred color
     const targetColor2 = '#00FF00';
     // Set to true if you want to change the color of your friend's chat messages.
@@ -44,7 +44,7 @@
 
     // Target Users: BLOCK LIST
     // Enter the usernames of the users you want blocked
-    const targetUsers3 = ['Kimahri_Ronso', 'DannyPhantom', 'BigBongoTheRealFren', 'michael'];
+    const targetUsers3 = ['BLOCK1', 'BLOCK2', 'BLOCK3', 'BLOCK4'];
     // This is blocked username color. Currently it's a darker blue but still visible. Change it to your taste or use #1d2233 to make it imperceptible
     const targetColor3 = '#1d4d75';
     // This makes the blocked users chat imperceptible to the eye. Can still be selected with the cursor if need of read
